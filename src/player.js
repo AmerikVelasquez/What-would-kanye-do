42c067534c554c1184cce17dc33d0308c36f7ed0
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-15 12:38:04 -0700

    add player constructor

---
+++ src/player.js
@@ -0,0 +1,13 @@
+class Player {
+  constructor(name, score, turn) {
+    this.name = name;
+    this.score = 0;
+    this.turn = false;
+  }
+  blockLetter = () => {
+
+  } 
+  revealLetter = () => {
+
+  }
+}
