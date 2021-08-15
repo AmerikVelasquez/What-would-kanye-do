d9b1efd2762985f8714b2a970e512dbf1f2c4b2f
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-15 12:40:32 -0700

    add alphabet array, changeturn function, and import other js files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Kanye from './kanye.js';
+import Player from './player.js';
+
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+changeTurn = () => {
+  if (this.turn === false) {
+    return true
+  } else {
+    return false
+  }
+}
