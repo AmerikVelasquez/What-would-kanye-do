5464547dd207b8f27817ca7842e17c12f8a11912
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-16 19:52:12 -0700

    move functions and variables to other files

--- src/index.js
+++ src/index.js
@@ -5,14 +5,5 @@ import './css/styles.css';
 import Kanye from './kanye.js';
 import Player from './player.js';
 
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-changeTurn = () => {
-  if (this.turn === false) {
-    return true
-  } else {
-    return false
-  }
-}
+
