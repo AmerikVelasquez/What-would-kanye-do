ac7d875c52f3ef0bbf1879a0b101c488ed721bb7
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-08-17 20:59:49 -0700

    add api to array push functionality

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,20 +1,28 @@
-import KanyeQuote from "./kanye.js";
+import Kanye from "./kanye.js";
 
-class KanyeHangman {
-  constuctor(lives) {
-    this.lives = lives;
+export default class KanyeHangman {
+  constructor() {
+    this.lives = 12;
     this.alreadyUsedQuotes = [];
     this.correctGuessCounter = 0;
   }
-  getKanyeQuote = () => {
-    let gottenQuote = KanyeQuote.getQuote();
-    this.alreadyUsedQuotes.push(gottenQuote);
-    let phrase = gottenQuote.split('');
-  }
-  trackRoundguess = () => {
 
+  async getKanyeQuote() {
+    let response = await (Kanye.getQuote());
+    // let phrase = gottenQuote.split('');
+    return response.quote;
   }
-  trackAllGuesses = () => {
 
+  async pushKanye(){
+  this.alreadyUsedQuotes.push((await this.getKanyeQuote()));
+  console.log(this.alreadyUsedQuotes)
   }
+
+  
+  // trackRoundguess = () => {
+
+  // }
+  // trackAllGuesses = () => {
+
+  // }
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import Kanye from './kanye.js';
+// import Kanye from './kanye.js';
 //import Player from './player.js';
+import KanyeHangman from './gameboard.js';
+
+  // let kanye = new KanyeHangman
+  // console.log(kanye.getKanyeQuote);
 
 $(document).ready(function() {
     const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
     'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z','BLOCK','RANDOM'];
     alphabet.forEach(function(element) {
-      $('ul#alphaList').append('<li>' + element + '</li>');
+      $('ul#alphaList').append(`<li id="${element}">${element}</li>`);
     })
+    let kanye = new KanyeHangman();
+
+  $('button').click(function(){
+    
+    kanye.pushKanye(kanye.getKanyeQuote());
+    // console.log(kanye.alreadyUsedQuotes);
+  })
 })
- 
