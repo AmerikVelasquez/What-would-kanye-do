07938ab70b732e710106403b099953eb2dc430bc
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-18 21:25:13 -0700

    add update Kanye function

--- src/index.js
+++ src/index.js
@@ -6,27 +6,49 @@ import './css/styles.css';
 //import Player from './player.js';
 import KanyeHangman from './gameboard.js';
 
-//front end
-async function concealKanye(kanye) {
-  let response = await kanye.getKanyeQuote();
-  let abcReg = /[abcdefghijklmnopqrstuvwxyz]/gi;
-  let concealedStr = response.replaceAll(" ", "-");
-  concealedStr = concealedStr.replace(abcReg, " _");
-  $('h2#result').text(concealedStr);
-  return concealedStr;
-}
-
 $(document).ready(function () {
+  let concealedStr = null;
+  let quote = null;
+  let usedAlphaReg = ('[ABCDEFGHIJKLMNOPQRSTUVWXYZ]');
+  // let tempReg = null;
+
+  //initial conceal for quote once generate phrase is clicked
+  async function concealKanye(kanye) {
+    let response = await kanye.getKanyeQuote();
+    quote = response;
+    let abcReg = /[abcdefghijklmnopqrstuvwxyz]/gi;
+    concealedStr = response.replaceAll(" ", "-");
+    concealedStr = concealedStr.replace(abcReg, "_");
+    $('h2#result').text(concealedStr);
+    return concealedStr;
+  }
+
+  //function for updating quote reg on char keyboard press
+  function updateKanye(attr) {
+    usedAlphaReg = usedAlphaReg.replace(attr, '');
+    let tempReg = new RegExp(usedAlphaReg, "gi");
+    concealedStr = quote
+    concealedStr = concealedStr.replaceAll(tempReg, "_");
+    concealedStr = concealedStr.replaceAll(" ", "-");
+    $('h2#result').text(concealedStr);
+  }
   const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
     'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'BLOCK', 'RANDOM'];
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   alphabet.forEach(function (element) {
-    $('ul#alphaList').append(`<li id="${element}">${element}</li>`);
+    $('ul#alphaList').append(`<li id="${element}" class="letter">${element}</li>`);
   })
   let kanye = new KanyeHangman();
 
-  $('button').click(function () {
+  $('#gen').click(function () {
+    $('button#gen').remove();
     kanye.pushKanye(kanye.getKanyeQuote());
     concealKanye(kanye);
+  })
+  $('.letter').click(function () {
+    this.remove();
+    let attr = this.getAttribute('id');
+    // console.log(attr)
+    updateKanye(attr)
   })
 })
