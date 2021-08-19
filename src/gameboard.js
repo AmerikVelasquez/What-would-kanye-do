d274ed00934d6c30e7e8ec6d4d3a471e1079c6cf
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-18 20:02:10 -0700

    add blank kanye function and add functionality for no duplicates

--- src/gameboard.js
+++ src/gameboard.js
@@ -5,24 +5,43 @@ export default class KanyeHangman {
     this.lives = 12;
     this.alreadyUsedQuotes = [];
     this.correctGuessCounter = 0;
+    this.getPhrase = [];
+    this.blankPhrase = "";
   }
 
   async getKanyeQuote() {
     let response = await (Kanye.getQuote());
     // let phrase = gottenQuote.split('');
+    //console.log(response.quote);
     return response.quote;
   }
 
-  async pushKanye(){
-  this.alreadyUsedQuotes.push((await this.getKanyeQuote()));
-  console.log(this.alreadyUsedQuotes)
+  async pushKanye() {
+    let response = await (this.getKanyeQuote());
+    // console.log(response);
+    if (this.alreadyUsedQuotes.includes(response)) {
+      this.pushKanye();
+    } else {
+      this.alreadyUsedQuotes.push(response);
+    }
+    return response;
+    //console.log(this.alreadyUsedQuotes);
   }
 
-  
+  async blankKanye() {
+    let next = await (this.pushKanye());
+    this.getPhrase.push(next);
+    //this.blankPhrase.push(next);
+    return this.blankPhrase;
+    //  console.log(this.getPhrase);
+    //  console.log(this.blankPhrase);
+  }
+
+}
   // trackRoundguess = () => {
 
   // }
   // trackAllGuesses = () => {
 
   // }
-}
+// }
