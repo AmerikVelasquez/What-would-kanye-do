3a532da791d9dfaff914be52ce9a382186455276
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-22 16:58:56 -0700

    fix errors

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import './css/styles.css';
 // import { Player } from './player.js';
 import  KanyeHangman  from './gameboard.js';
   
-  let kanye = new KanyeHangman();
-  let blockMode = false;
+let kanye = new KanyeHangman();
+let blockMode = false;
 
 $(document).ready(function () {
+  $('#kanye-pics').append('<img src="../img/kanye0.png">')
   let concealedStr = null;
   let quote = null;
   let usedAlphaReg = ('[ABCDEFGHIJKLMNOPQRSTUVWXYZ]');
@@ -30,38 +31,41 @@ $(document).ready(function () {
   function showGameState(attr) {
     usedAlphaReg = usedAlphaReg.replaceAll(attr, '');
     let tempReg = new RegExp(usedAlphaReg, "gi");
-    concealedStr = quote
+    concealedStr = quote;
     concealedStr = concealedStr.replaceAll(tempReg, "_");
     concealedStr = concealedStr.replaceAll(" ", "-");
     $('h2#result').text(concealedStr);
     //  }
-   }
+  }
 
   const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
     'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   alphabet.forEach(function (element) {
     $('ul#alphaList').append(`<li id="${element}" class="letter">${element}</li>`);
-  })
+  });
 
 
   $('#gen').click(function () {
-    //$('button#gen').remove();
+    $('button#gen').remove();
     $('#random').show();
     $('#block').show();
     kanye.duplicate(kanye.getKanyeQuote());
     concealQuote(kanye); 
-  })
+  });
 
   $('.letter').click(function () {
     if(blockMode === true) {
-      this.remove()
+      this.remove();
       blockMode = false;
     }else {
       this.remove();
       let attr = this.getAttribute('id');
-      console.log(attr)
+      console.log(attr);
       showGameState(attr);
+      for(let i = 0; i < alphabet.length; i++){ //needs fixing
+        kanye.booLean[i] = false;
+      }
     } 
   });
 
@@ -71,10 +75,11 @@ $(document).ready(function () {
     letter = letter.toUpperCase();
     $(`#${letter}`).removeClass('letter').addClass('revealed');
     this.remove();
-  })
+    console.log(kanye.booLean);
+  }); //needs fixing
 
-  $('#block').click(function() {
-   blockMode = true;
-   this.remove();
-  })
-})
+  $('#block').click(function() { //works
+    blockMode = true;
+    this.remove();
+  });
+});
