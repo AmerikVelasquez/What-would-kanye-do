0b5741b9b4570415d63a165dae277b01c4ce9a86
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-23 21:06:27 -0700

    add track wrong guess function and reveal kanye function

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,6 +4,8 @@ export default class KanyeHangman {
   constructor() {
     this.lives = 10;
     this.alreadyUsedQuotes = [];
+    this.thePhrase = "";
+    this.correctGuessCounter = 0;
     this.wrongGuessCounter = 0;
     this.phrase = [];
     this.booLean = [];
@@ -11,39 +13,29 @@ export default class KanyeHangman {
 
   async getKanyeQuote() {
     let response = await (Kanye.getQuote());
-    // let phrase = gottenQuote.split('');
-    //console.log(response.quote);
     return response.quote;
   }
 
   async duplicate() {
     let response = await (this.getKanyeQuote());
-    // console.log(response);
     if (this.alreadyUsedQuotes.includes(response)) {
       this.duplicate();
     } else {
       this.alreadyUsedQuotes.push(response);
     }
-    return response;
-    //console.log(this.alreadyUsedQuotes);
-  }
-
-  async blankKanye() {
-    let next = await (this.duplicate());
-    next = next.replaceAll(/[^A-z]+/gi, '');
-    this.phrase = next.split('');
+    response = response.replaceAll(/[^A-z]+/gi, '');
+    this.thePhrase = response;
+    this.phrase = response.split('');
     for(let i = 0; i < this.phrase.length; i++) {
       this.booLean.push(true);
     }
-    return next;
+    return response;
   }
   
-  async revealLetter() { //broken needs fixing
-    let max = await this.blankKanye();
+  revealLetter() { //broken needs fixing
+    let max = this.thePhrase;
     let random = Math.floor(Math.random() * max.length);
     let test = max[random];
-    console.log(this.booLean);
-    console.log(test);
     if(this.booLean[random] === false) {
       this.revealLetter();
     }else {
@@ -51,6 +43,18 @@ export default class KanyeHangman {
       return test;
     }
   }
+  trackWrongGuess (attr) {
+    console.log(this.thePhrase);
+    console.log(attr);
+    if(this.thePhrase.includes(attr)) {
+      this.correctGuessCounter + 1;
+    } else {
+      this.wrongGuessCounter = this.wrongGuessCounter + 1;
+      this.lives = this.lives - 1;
+      console.log(this.wrongGuessCounter);
+    }
+  }
+
 
   // revealKanye() {
   //   var kanyeArray = ["kanye0.png","kanye1.png", "kanye2.png", "kanye3.png", "kanye4.png", "kanye5.png", "kanye6.png", "kanye7.png", "kanye8.png", "kanye9.png", "kanye10.png"]
@@ -65,8 +69,68 @@ export default class KanyeHangman {
   //     //show kanye10 and play audio clip of "you aint got the answers sway"
   //   }
   // }
+
+  revealKanye() {
+    var imgArray = new Array();
+
+    imgArray[0] = new Image();
+    imgArray[0].src = '../assets/kanye0.png';
+    
+    imgArray[1] = new Image();
+    imgArray[1].src = '../assets/kanye1.png';
+
+    imgArray[2] = new Image();
+    imgArray[2].src = '../assets/kanye2.png';
+
+    imgArray[3] = new Image();
+    imgArray[3].src = '../assets/kanye3.png';
+
+    imgArray[4] = new Image();
+    imgArray[4].src = '../assets/kanye4.png';
+
+    imgArray[5] = new Image();
+    imgArray[5].src = '../assets/kanye5.png';
+
+    imgArray[6] = new Image();
+    imgArray[6].src = '../assets/kanye6.png';
+
+    imgArray[7] = new Image();
+    imgArray[7].src = '../assets/kanye7.png';
+
+    imgArray[8] = new Image();
+    imgArray[8].src = '../assets/kanye8.png';
+    
+    imgArray[9] = new Image();
+    imgArray[9].src = '../assets/kanye9.png';
+
+    imgArray[10] = new Image();
+    imgArray[10].src = '../assets/kanye10.png';
+
+    var img = document.getElementById();
+
+    for(var i = 0; i <imgArray.length; i++) {
+
+      if(imgArray[i].src == img.src){
+
+        if(i === imgArray.length/* && this.answer === false*/){
+          document.getElementById().src = imgArray[0].src;
+          /*this.wrongGuessCounter += 1; this.lives -= 1; */
+          break;
+        }
+        document.getElementById().src = imgArray[i+1].src;
+        break;
+      }
+    }
+  }
+
+  revealKanyeTwo() {
+    if(this.answer === false){
+      this.wrongGuessCounter +=1; this.lives -=1;
+      this.revealKanye(/*imgArray[i+1]*/);
+    }
+  }
 }
-// trackRoundguess = () => {
+// trackRoundguess = {}() => {
 
 // }
 // trackAllGuesses = () => {
