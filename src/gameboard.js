b8c59195a3a4deee8b3b334555ebd1e27bd33c3e
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-25 20:01:59 -0700

    add get unique character function,remove, and guess functions
    Co-authored-by: Aaron Kauffman <aaron.christian.kauffman@gmail.com>
    Co-authored by: Andre Velasquez <andrethefirst21@gmail.com>
    Co-authored-by: Shaun Kent <shaunkent81@gmail.com>

--- src/gameboard.js
+++ src/gameboard.js
@@ -7,147 +7,100 @@ export default class KanyeHangman {
     this.wrongGuessCounter = 0;
   }
 
+  getUniqChars(str) {
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    this.uniqChars = "";
+    str = str.toLowerCase();
+    str = str.replaceAll(" ", "");
+    let arr = str.split("");
+    let charIndex = null;
+    alphabet.forEach(element => {
+      charIndex = arr.indexOf(element);
+      if (charIndex !== -1) {
+        this.uniqChars = this.uniqChars.concat(arr[charIndex]);
+      }
+    });
+  }
+
   async getQuote() {
     try {
       const response = await fetch(`https://api.kanye.rest`);
-      if (!response.ok){
+      if (!response.ok) {
         throw Error(response.statusText);
-
       }
       return response.json();
-      } catch (error) {
+    } catch (error) {
       return error.message;
     }
   }
 
-  trackWrongGuess (attr) {
-    if(this.quote.includes(attr)) {
+  trackWrongGuess(attr) {
+    if (this.quote.includes(attr)) {
       this.correctGuessCounter + 1;
     } else {
       this.wrongGuessCounter = this.wrongGuessCounter + 1;
       this.lives = this.lives - 1;
     }
   }
-  calculateScore() {//put calculate score at next round button
+  calculateScore() {
     let nonAlphaQuote = this.quote.replaceAll(/[^A-z]/gi, '');
     let roundScore = Math.floor(nonAlphaQuote.length / 2);
     return roundScore;
   }
 
+  removeUniqChar(letter){
+    letter = letter.toLowerCase();
+    this.uniqChars = this.uniqChars.replace(letter, '');
+  }  
+
   chooseRandom(str) {
-    if(this.uniqChars){
+    if (this.uniqChars) {
       let int = Math.floor(Math.random() * this.uniqChars.length);
       let randomChar = this.uniqChars.charAt(int);
       this.uniqChars = this.uniqChars.replace(randomChar, '');
       return randomChar;
     } else {
-      //the blank array/str that will be returned
       let uniqChars = "";
-      //predefined alphabet
       let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-      //manipulate string given by our api
       str = str.toLowerCase();
       str = str.replaceAll(" ", "");
-      //split it into a usable array
       let arr = str.split("");
-      //variable for use in forEach
       let int = null;
       let charIndex = null;
-      //forEach alphabet index
       alphabet.forEach(element => {
         charIndex = arr.indexOf(element);
         if (charIndex !== -1) {
           uniqChars = uniqChars.concat(arr[charIndex]);
         }
       });
-      //get integer between uniqCharacters list
       int = Math.floor(Math.random() * uniqChars.length);
-      //create key-value pair in object
       this.uniqChars = uniqChars;
-      //store chosen random character to delete from uniqChar string, and finally return
       let randomChar = uniqChars.charAt(int);
-      //delete randomly selected character from unique characters in our phrase
       this.uniqChars = this.uniqChars.replace(randomChar, '');
-
-      //reuturn <3
       return randomChar;
     }
   }
 
-
-  // revealKanye() {
-  //   var kanyeArray = ["kanye0.png","kanye1.png", "kanye2.png", "kanye3.png", "kanye4.png", "kanye5.png", "kanye6.png", "kanye7.png", "kanye8.png", "kanye9.png", "kanye10.png"]
-  //   for (let i=0; i<this.wrongGuessCounter; i++)
-  //   {
-  //    if(this.answer === false) {
-  //     this.wrongGuessCounter += 1;  this.lives -=1;
-  //    // display img in kanye array?
-  //    }
-  //   }
-  //   if(this.wrongGuessCounter === 10 && this.lives === 0) {
-  //     //show kanye10 and play audio clip of "you aint got the answers sway"
-  //   }
-  // }
-
-  revealKanye() {
-    var imgArray = new Array();
-
-    imgArray[0] = new Image();
-    imgArray[0].src = 'https://imgur.com/Nz9raO0';
+  guess(playerGuess){
+    this.quoteKey = this.quote.replaceAll(' ', '-');
+    this.quoteKey;
+    this.quoteKey = this.quoteKey.toLowerCase();
+    if(playerGuess === this.quoteKey){
+      return true;
+    } else {
+      return false;
     
-    imgArray[1] = new Image();
-    imgArray[1].src = 'https://imgur.com/LOcbnyC';
-
-    imgArray[2] = new Image();
-    imgArray[2].src = 'https://imgur.com/7CkSsln';
-
-    imgArray[3] = new Image();
-    imgArray[3].src = 'https://imgur.com/OzeOAjq';
-
-    imgArray[4] = new Image();
-    imgArray[4].src = 'https://imgur.com/OLHppzR';
-
-    imgArray[5] = new Image();
-    imgArray[5].src = 'https://imgur.com/jUkzUXi';
-
-    imgArray[6] = new Image();
-    imgArray[6].src = 'https://imgur.com/9a2kbmk';
-
-    imgArray[7] = new Image();
-    imgArray[7].src = 'https://imgur.com/ftfHV8d';
-
-    imgArray[8] = new Image();
-    imgArray[8].src = 'https://imgur.com/6UhIMvv';
-    
-    imgArray[9] = new Image();
-    imgArray[9].src = 'https://imgur.com/KlONx88';
-
-    imgArray[10] = new Image();
-    imgArray[10].src = 'https://imgur.com/0inJgCQ';
-
-    var img = document.getElementById();
-
-    for(var i = 0; i <imgArray.length; i++) {
-
-      if(imgArray[i].src == img.src){
-
-        if(i === imgArray.length/* && this.answer === false*/){
-          document.getElementById().src = imgArray[0].src;
-          /*this.wrongGuessCounter += 1; this.lives -= 1; */
-          break;
-        }
-        document.getElementById().src = imgArray[i+1].src;
-        break;
-      }
     }
   }
-
-  revealKanyeTwo() {
-    if(this.answer === false){
-      this.wrongGuessCounter +=1; this.lives -=1;
-      this.revealKanye(/*imgArray[i+1]*/);
+  revealKanye(){
+    let kanyeArray= ['https://imgur.com/Nz9raO0.png', 'https://imgur.com/LOcbnyC.png','https://imgur.com/7CkSsln.png', 'https://imgur.com/OLHppzR.png','https://imgur.com/9a2kbmk.png', 'https://imgur.com/6UhIMvv.png', 'https://imgur.com/KlONx88.png', 'https://imgur.com/0inJgCQ.png'];
+    return kanyeArray[this.wrongGuessCounter];
+  }
+  winLoss () {
+    if(this.wrongGuessCounter === 7) {
+      return false;
+    }else if(this.uniqChars === "") {
+      return true;
     }
   }
-  
 }
-
